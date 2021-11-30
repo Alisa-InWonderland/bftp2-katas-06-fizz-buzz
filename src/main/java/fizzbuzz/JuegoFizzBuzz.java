a6d6d89db71db01fd438895f13c6931b6019b7b1
Alisa-InWonderland <alisa_ne@yahoo.com>
2021-11-30 10:19:14 +0100

    hecho

--- src/main/java/fizzbuzz/JuegoFizzBuzz.java
+++ src/main/java/fizzbuzz/JuegoFizzBuzz.java
@@ -3,6 +3,19 @@ package fizzbuzz;
 public class JuegoFizzBuzz {
 
     public String responder(int numero) {
-        return String.valueOf(numero);
+
+        if (numero % 3 == 0 && numero % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (numero % 3 == 0) {
+            return "Fizz";
+        }
+        if (numero % 5 == 0) {
+            return "Buzz";
+        }
+        else {
+            return String.valueOf(numero);
+        }
     }
 }
--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -24,4 +24,70 @@ public class FizzBuzzTest {
         String respuesta = juego.responder(2);
         assertThat(respuesta, equalTo("2"));
     }
+
+    @Test
+    void laRespuestaA3EsFizz() {
+        String respuesta = juego.responder(3);
+        assertThat(respuesta, equalTo("Fizz"));
+    }
+
+    @Test
+    void laRespuestaA5EsFizz() {
+        String respuesta = juego.responder(5);
+        assertThat(respuesta, equalTo("Buzz"));
+    }
+
+    @Test
+    void laRespuestaA6EsFizz() {
+        String respuesta = juego.responder(6);
+        assertThat(respuesta, equalTo("Fizz"));
+    }
+
+    @Test
+    void laRespuestaA9EsFizz() {
+        String respuesta = juego.responder(9);
+        assertThat(respuesta, equalTo("Fizz"));
+    }
+
+    @Test
+    void laRespuestaA10EsFizz() {
+        String respuesta = juego.responder(10);
+        assertThat(respuesta, equalTo("Buzz"));
+    }
+
+    @Test
+    void laRespuestaA12EsFizz() {
+        String respuesta = juego.responder(12);
+        assertThat(respuesta, equalTo("Fizz"));
+    }
+
+    @Test
+    void laRespuestaA13EsFizz() {
+        String respuesta = juego.responder(13);
+        assertThat(respuesta, equalTo("13"));
+    }
+
+    @Test
+    void laRespuestaA15EsFizz() {
+        String respuesta = juego.responder(15);
+        assertThat(respuesta, equalTo("FizzBuzz"));
+    }
+
+    @Test
+    void laRespuestaA20EsFizz() {
+        String respuesta = juego.responder(20);
+        assertThat(respuesta, equalTo("Buzz"));
+    }
+
+    @Test
+    void laRespuestaA30EsFizz() {
+        String respuesta = juego.responder(30);
+        assertThat(respuesta, equalTo("FizzBuzz"));
+    }
+
+    @Test
+    void laRespuestaA45EsFizz() {
+        String respuesta = juego.responder(45);
+        assertThat(respuesta, equalTo("FizzBuzz"));
+    }
 }
